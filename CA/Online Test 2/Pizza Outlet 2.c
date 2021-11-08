8be0eb777ebb5a9b946573698144c8c312fc6197
Pasindu Dinal Sumanasekara <51123579+mrmihi@users.noreply.github.com>
2021-11-08 18:27:35 +0530

    Create Pizza Outlet 2.c

---
+++ CA/Online Test 2/Pizza Outlet 2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int p_type, quantity;
+	
+	printf("\nEnter Pizza Type \t: ");
+	scanf("%i",&p_type);
+	
+	while(p_type != -1) 
+	{
+
+		printf("Enter Quantity \t\t: ");
+		scanf("%i",&quantity);
+		printf("Total Price \t\t: ");
+		 switch(p_type){
+		 	case 1:	
+		 			if (quantity > 3)
+		 				printf("Rs.%.2f \n",(quantity*1000.0)*(1 - 0.1));
+		 			else
+		 				printf("RS.%.2f \n",(quantity*1000.0));
+		 			break;
+		 
+		 	case 2:	
+		 			if (quantity > 3)
+		 				printf("Rs.%.2f \n",(quantity*1600.0)*(1 - 0.12));
+		 			else
+		 				printf("RS.%.2f \n",(quantity*1600.0));
+		 			break;
+		 	
+		 	case 3:	
+		 			if (quantity > 3)
+		 				printf("Rs.%.2f \n",(quantity*1400.0)*(1 - 0.15));
+		 			else
+		 				printf("RS.%.2f \n",(quantity*1400.0));
+		 			break;
+		 }
+		 
+		printf("\nEnter Pizza Type \t: ");
+		scanf("%i",&p_type);
+	}
+}
